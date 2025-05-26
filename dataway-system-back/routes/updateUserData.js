2252f44f4db70349f99766c497e1b2ede84a3812
andradeGabriel05 <gabriel.andrade@sptech.school>
2025-05-25 21:21:26 -0300

    v0.1.4.6.1.1 refactor user data handling: update updateUserData to include nome and senha

--- dataway-system-back/routes/updateUserData.js
+++ dataway-system-back/routes/updateUserData.js
@@ -6,7 +6,7 @@ var updateUserData = require("../controllers/updateUserDataController");
 //Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
 router.put("/", function (req, res) {
   const idUsuario = req.query.idUsuario; 
-  updateUserData.updateUserData(req, res,  idUsuario);
+  updateUserData.updateUserData(req, res, idUsuario);
 });
 
 module.exports = router;
