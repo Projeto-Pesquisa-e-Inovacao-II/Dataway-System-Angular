880c2776ea024d9f22ae405bdb84c353f64d5697
glandrade1 <glandrade1@stefanini.com>
2025-05-29 23:09:10 -0300

    v0.1.4.9.1 (adm) enhance header layout and styling; add search input with icon;

--- dataway-system-front/src/app/pages/create-empresas-adm/create-empresas-adm.component.ts
+++ dataway-system-front/src/app/pages/create-empresas-adm/create-empresas-adm.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header/header.component";
 
 interface Empresa {
   
